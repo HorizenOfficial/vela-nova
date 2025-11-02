2800bd5aae5e136dbdc30656609d9c81417d6e51
ANDREA NISTICO <nistic.andrea@gmail.com>
2025-11-03 00:40:28 +0100

    add config to new deploy

--- wallet/cmd/root.go
+++ wallet/cmd/root.go
@@ -31,7 +31,7 @@ func Execute() {
 	rootCmd.AddCommand(NewGetPrivateBalanceCommand(config, nil).Command())
 	rootCmd.AddCommand(NewDecryptReportCommand(config, nil).Command())
 	rootCmd.AddCommand(NewWithdrawCommand(config, nil).Command())
-  rootCmd.AddCommand(NewDeployAppCommand(nil, nil).Command())
+  	rootCmd.AddCommand(NewDeployAppCommand(config, nil).Command())
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
